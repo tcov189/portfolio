8e56551150e514823eb83d6944867386edbfd0f5
Trevor Covington <tcovington189@gmail.com>
2021-06-15 14:21:58 -0500

    Added CTM for experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -16,13 +16,25 @@ export default {
     return {
       jobs: [
         {
+          title: "Software Development Engineer II",
+          subtitle: "Corporate Travel Management US",
+          time: "April 2021 - Present",
+          description: [
+            "Part of a team that engineered web platforms/services that facilitated travel reward redemption and management for multiple clients.",
+            "Developed and maintained PHP/Laravel APIs that provided SSO integration with clients via SAML and Auth2.0.",
+            "Worked with React front end applications.",
+          ],
+        },
+        {
           title: "Software Developer",
           subtitle: "Hyphen Financial",
-          time: "September 2020 - Present",
+          time: "September 2020 - April 2021",
           description: [
-            "Worked remotely with a team to modernize loan application software.",
-            "Converted Code Igniter based application to Laravel.",
-            "Developed an admin application utilizing Vue/Vuetify SPA with Laravel API powered back end.",
+            "Worked with a remote team to facilitate web based loan application software.",
+            "Developed Vue JS front end applications to suit both customer facing and internal applications.",
+            "Engineered PHP (Laravel) APIs to provide data to front end apps.",
+            "Utilized MySQL for application data architecture.",
+            "Practiced Test Driven Development to produce quality and error resistant code.",
           ],
         },
         {
@@ -31,10 +43,9 @@ export default {
           time: "July 2017 - September 2020",
           description: [
             "Created custom web applications for a wide variety of projects using mostly PHP for server side operations and MySql for database administration.",
-            "Developed custom web applications.",
+            "Developed and maintained custom CMS platform using Laravel and Vue.",
             "Spearheaded effort to utilize Git Flow as our source management strategy..",
             "Developed and implemented SOC 2 compliant automated deployment mechanism.",
-            "Developed and maintained custom CMS platform using Laravel.",
           ],
         },
         {
@@ -42,8 +53,8 @@ export default {
           subtitle: "FabPro",
           time: "July 2018 - September 2018",
           description: [
-            "Worked on legacy PHP application.",
             "Developed Express JS based API for a React front end.",
+            "Worked on legacy PHP application.",
           ],
         },
         {
@@ -62,7 +73,7 @@ export default {
           time: "June 2014 - January 2015",
           description: [
             "Developing projects utilizing languages such as HTML5, CSS3 and jQuery/JavaScript along with back end technologies including PHP and SQL.",
-            "Working with team to update and maintain several sites including working with Wordpress and Yahoo Small Business sites..",
+            "Working with team to update and maintain several sites including working with Wordpress and Yahoo Small Business sites.",
             "Design and development of product and promotion pages.",
             "Maintaining proper file management including consistent nomenclature and hierarchy.",
             "Create and optimize images for proper balance between file size and image quality.",
